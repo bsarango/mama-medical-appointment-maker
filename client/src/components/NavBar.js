7751917dc98f931b185f2ea79fcbfb7c41d36ba0
Brian Sarango <brian.sarango@baruchmail.cuny.edu>
2024-05-31 18:05:22 -0400

    Navlink imported to NavBar component from react-router-dom; NavLinks created for physicians_index, patient_profile, signup, and login

---
+++ client/src/components/NavBar.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import {NavLink} from 'react-router-dom';
+
+function NavBar(){
+    return(
+        <nav>
+            <NavLink to="/"
+                exact
+            >
+                Home
+            </NavLink>
+            <NavLink to="/physicians_index"
+                exact
+            >
+                Physicians
+            </NavLink>
+            <NavLink to="/patient_profile"
+                exact
+            >
+                My Profile
+            </NavLink>
+            <NavLink to="/signup"
+                exact
+            >
+                Signup
+            </NavLink>
+            <NavLink to="/login"
+                exact
+            >
+                Login
+            </NavLink>
+        </nav>
+    )
+}
+
+export default NavBar;
