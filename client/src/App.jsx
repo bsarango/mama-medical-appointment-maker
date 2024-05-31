d0bad7c2ea7d2fd85f7eb288ec8234bb3f43b4fe
Brian Sarango <brian.sarango@baruchmail.cuny.edu>
2024-05-31 13:00:40 -0400

    useEffect imported to App.jsx; count state variable updated to user; useEffect imported and function defined in App.jsx to check session

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,19 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [user, setUser] = useState(null)
   
+  useEffect(()=>{
+    fetch("/check_session")
+    .then(r =>{
+      if (r.ok){
+        r.json().then(user=>setUser(user));
+      }
+    });
+  },[]);
 
   return (
     <>
