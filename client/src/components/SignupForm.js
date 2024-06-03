ea60a908b670707f23bb91eb26f0f7af490ee182
Brian Sarango <brian.sarango@baruchmail.cuny.edu>
2024-06-03 13:02:47 -0400

    Inputs for form created in body of SignupForm component

--- client/src/components/SignupForm.js
+++ client/src/components/SignupForm.js
@@ -8,8 +8,9 @@ function SignupForm(){
         name : "",
         dob : "",
         address : "",
-        phone_number : "",
+        phoneNumber : "", 
     };
+{/*Don't forget to adjust phoneNumber key syntax when fetching to POST in backend */}
 
     const [newPatientInfo, setNewPatientInfo] = useState(initialValues)
 
@@ -28,8 +29,53 @@ function SignupForm(){
 
     return(
         <div>
+            <form className = "userForm">
+                <label>Username</label>
+                <input
+                    type = "text"
+                    name = "username"
+                    value = {newPatientInfo.username}
+                    onChange = {handleChange}
+                />
+                <label>Password</label>
+                <input
+                    type = "text"
+                    name = "password"
+                    value = {newPatientInfo.password}
+                    onChange = {handleChange}
+                />
+                <label>Full Name</label>
+                <input
+                    type = "text"
+                    name = "name"
+                    value = {newPatientInfo.name}
+                    onChange = {handleChange}
+                />
+                <label>Username</label>
+                <input
+                    type = "text"
+                    name = "username"
+                    value = {newPatientInfo.username}
+                    onChange = {handleChange}
+                />
+                {/*Add input for DOB */}
+                <label>Address</label>
+                <input
+                    type = "text"
+                    name = "address"
+                    value = {newPatientInfo.address}
+                    onChange = {handleChange}
+                />
+                <label>Phone Number</label>
+                <input
+                    type = "text"
+                    name = "phoneNumber"
+                    value = {newPatientInfo.phoneNumber}
+                    onChange = {handleChange}
+                />
+            </form>
         </div>
     );
-}
+};
 
 export default SignupForm
