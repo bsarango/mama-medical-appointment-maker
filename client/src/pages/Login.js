285af5b3cbb0a2e9989001ea1aa9f13c5509e2c5
Brian Sarango <brian.sarango@baruchmail.cuny.edu>
2024-06-03 13:48:05 -0400

    div created for body of Login.js; LoginForm imported and added to body; loggedIn, setLoggedIn, and patient set as props

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,10 +1,18 @@
 import React,{useEffect, setState} from 'react'
 import LoginForm from '../components/LoginForm'
 
-function Login(){
+function Login({loggedIn, setLoggedIn, patient}){
 
     //Have some conditional to check that the user if logged in
-    {/* if (isLoggedIn) {return(<h3>Hi {patient.name}. You're already logged in!</h3>)}*/}
+    {/* if (loggedIn) {return(<h3>Hi {patient.name}. You're already logged in!</h3>)}*/}
 
-    
+    return(
+        <div>
+            <p>Enter your credentials below and press submit to Sign In</p>
+            <LoginForm setLoggedIn={setLoggedIn} patient={patient}/>
+        </div>
+    );
+
 }
+
+export default Login
