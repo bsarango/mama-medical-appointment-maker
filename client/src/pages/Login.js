fdcf299fa10926f6a2fde7a5bd1bf5740c761113
Brian Sarango <brian.sarango@baruchmail.cuny.edu>
2024-06-04 12:55:51 -0400

    h3 tag removed in Login.js

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -8,7 +8,7 @@ function Login({loggedIn, setLoggedIn, patient}){
 
     return(
         <div>
-            <h3>Enter your credentials below and press submit to Sign In</h3>
+            {/* <h3>Enter your credentials below and press submit to Sign In</h3> */}
             <LoginForm setLoggedIn={setLoggedIn} patient={patient}/>
         </div>
     );
