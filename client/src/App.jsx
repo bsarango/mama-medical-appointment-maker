2b2e57f53c2fcd0c8612bef47b458b78b6239cfd
Brian Sarango <brian.sarango@baruchmail.cuny.edu>
2024-06-03 23:04:39 -0400

    paths in router in main.jsx changed to be children of App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -47,7 +47,7 @@ function App() {
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
-      </p> */}
+      </p> */} 
     </header>
   );
 }
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -5,20 +5,32 @@ import App from './App.jsx'
 import Login from './pages/Login.js'
 import Signup from './pages/Signup.js'
 import './index.css'
+import AppointmentRequest from './pages/AppointmentRequest.js'
 
 const router = createBrowserRouter ([
   {
     path:"/",
-    element: <App/>
-  },
-  {
-    path:"/signup",
-    element: <Signup/>
-  },
-  {
-    path:"/login",
-    element: <Login/>
+    element: <App/>,
+    children: [
+      {
+        path:"/",
+        element:<Home/>
+      },
+      {
+        path:"/signup",
+        element: <Signup/>
+      },
+      {
+        path:"/login",
+        element: <Login/>
+      },
+      {
+        path:"/make_appointment",
+        element: <AppointmentRequest/>
+      }
+    ]
   }
+  
 ])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
