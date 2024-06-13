1df1d55fde5d53de0b27a7be7f2a038b11f2f039
Brian Sarango <brian.sarango@baruchmail.cuny.edu>
2024-06-12 22:56:33 -0400

    css added to button in App.jsx and Navbar elements in Navbar component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -23,7 +23,7 @@ function App() {
       <header className = "bg-lime-200">
         <h2 className="font-semibold text-center">MaMa by My Medical Group </h2>
         <NavBar/>
-        <button className="bg-transparent hover:bg-emerald-900 text-green-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded" onClick={e=>{setLoggedIn(false)}}>LogOut</button>
+        <button className="bg-transparent hover:bg-emerald-900 text-green-700 font-semibold hover:text-white py-2 px-4 border border-green-300 hover:border-transparent rounded" onClick={e=>{setLoggedIn(false)}}>LogOut</button>
       </header>
       <Outlet context={{patient:[patient, setPatient],loggedIn:[loggedIn,setLoggedIn]}}/>
     </>
--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -3,34 +3,34 @@ import {NavLink} from 'react-router-dom'
 
 function NavBar(){
     return(
-        <nav >
+        <nav className="absolute top-7 right-0 h-16 w-30">
             <NavLink 
                 to="/"
-                className = "nav-link bg-gray-800"
+                className = "nav-link bg-green-300 hover:bg-emerald-900 text-stone-800 font-semibold hover:text-white py-2 px-4 border border-green-300 hover:border-transparent rounded"
             >
             Home
             </NavLink>
             <NavLink 
                 to="/physicians_index"
-                className = "nav-link"
+                className = "nav-link bg-green-300 hover:bg-emerald-900 text-stone-800 font-semibold hover:text-white py-2 px-4 border border-green-300 hover:border-transparent rounded"
             >
             Physicians
             </NavLink>
             <NavLink 
                 to="/patient_profile"
-                className = "nav-link"
+                className = "nav-link bg-green-300 hover:bg-emerald-900 text-stone-800 font-semibold hover:text-white py-2 px-4 border border-green-300 hover:border-transparent rounded"
             >
             My Profile
             </NavLink>
             <NavLink 
                 to="/signup"
-                className = "nav-link"
+                className = "nav-link bg-green-300 hover:bg-emerald-900 text-stone-800 font-semibold hover:text-white py-2 px-4 border border-green-300 hover:border-transparent rounded"
             >
             Signup
             </NavLink>
             <NavLink 
                 to="/login"
-                className = "nav-link"
+                className = "nav-link bg-green-300 hover:bg-emerald-900 text-stone-800 font-semibold hover:text-white py-2 px-4 border border-green-300 hover:border-transparent rounded"
             >
             Login
             </NavLink>
