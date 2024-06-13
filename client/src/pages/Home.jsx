4c211ffea3cf80b03ae4ef8fc157bea1247c907a
Brian Sarango <brian.sarango@baruchmail.cuny.edu>
2024-06-12 23:51:39 -0400

    css added to Home.jsx

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -7,10 +7,10 @@ function Home(){
     console.log(loggedIn[0])
     if(loggedIn[0]){
         return(
-            <main className = "container md:max-auto px-4 border-solid border-2 border-green-600 outline box-border h-64 w-64 p-4 border-4">
-                <h3>Appointment making system focused on helping you obtain the best care for you needs.
+            <main className = "container md:max-auto px-4 border-solid border-2 border-green-600 outline box-border h-64 w-80 p-4 border-4 text-center absolute inset-x-1/3 top-48">
+                <p className="text-lg">Appointment making system focused on helping you obtain the best care for you needs.
                     We care just like Mama did for you!
-                </h3>
+                </p>
                 <div className="object-center">
                     Please choose any of the following options for our services
                     <br></br>
@@ -29,11 +29,12 @@ function Home(){
 
     return(
         <>
-            <main className="container md:max-auto px-4 border-solid border-2 border-green-600 outline box-border h-64 w-80 p-4 border-4 ">
-                <h3 className="object-center">Appointment making system focused on helping you obtain the best care for you needs.
+            <main className="container md:max-auto px-4 border-solid border-2 border-green-600 outline box-border h-64 w-80 p-4 border-4 text-center absolute inset-x-1/3 top-48">
+                <h3 className="text-lg font-sans">Appointment making system focused on helping you obtain the best care for you needs.
                     We care just like Mama did for you!
                 </h3>
-                <div>
+                <br></br>
+                <div className = "text-base italic">
                     Please log in to use our services or check our physicians
                 </div>
             </main>
