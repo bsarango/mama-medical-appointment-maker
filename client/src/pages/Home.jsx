f2dfde19ab788caa9a1c427b639d05a0546b13bb
Brian Sarango <brian.sarango@baruchmail.cuny.edu>
2024-06-12 22:40:15 -0400

    tailwind css added to main in Home.js

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -7,11 +7,11 @@ function Home(){
     console.log(loggedIn[0])
     if(loggedIn[0]){
         return(
-            <main className = "container md:max-auto">
+            <main className = "container md:max-auto px-4 border-solid border-2 border-green-600 outline box-border h-64 w-64 p-4 border-4">
                 <h3>Appointment making system focused on helping you obtain the best care for you needs.
                     We care just like Mama did for you!
                 </h3>
-                <div>
+                <div className="object-center">
                     Please choose any of the following options for our services
                     <br></br>
                     <Link to="/make_appointment">
@@ -29,8 +29,8 @@ function Home(){
 
     return(
         <>
-            <main className="container mx-auto px-4">
-                <h3>Appointment making system focused on helping you obtain the best care for you needs.
+            <main className="container md:max-auto px-4 border-solid border-2 border-green-600 outline box-border h-64 w-80 p-4 border-4 ">
+                <h3 className="object-center">Appointment making system focused on helping you obtain the best care for you needs.
                     We care just like Mama did for you!
                 </h3>
                 <div>
